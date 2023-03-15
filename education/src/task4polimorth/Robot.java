3cb02cee9d3d4d33ee4f16f3fd950defa50e0ca7
AntoniukVitalii <antoniuk.vitalii@gmail.com>
2023-03-15 22:01:04 +0200

    second_commit_task4

--- education/src/task4polimorth/Robot.java
+++ education/src/task4polimorth/Robot.java
@@ -4,6 +4,7 @@ public class Robot extends Participant {
 
     public Robot(String name, int runLimit, int jumpLimit) {
         super(name, runLimit, jumpLimit);
+        this.setRunLimit(0);
     }
 
 }
